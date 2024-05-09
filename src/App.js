2a06b1604814c82e079973421f4d8fadb8140a28
hakmoune <mehdi.hakmoune@gmail.com>
2024-05-10 01:13:12 +0200

    useTrasition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useTransition } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [list, setlist] = useState([]);
+
+  // All the states inside the function "startTransition" is going to be low priority by default
+  const [isPending, startTransition] = useTransition();
+
+  const LIST_SIZE = 20000;
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+
+    startTransition(() => {
+      const l = [];
+      for (let i = 0; i < LIST_SIZE; i++) {
+        l.push(e.target.value);
+      }
+      setlist(l);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={input} onChange={handleChange} />
+      {isPending && <p>Loading...</p>}
+      {list.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
     </div>
   );
 }
